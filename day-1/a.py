a2b5a1898747315f700ee2f8afb9d43e32ed881a
Juanjo Zunino <juanj.zunino@gmail.com>
2023-12-08 10:57:27 -0300

    Shorter version of first problem

--- day-1/a.py
+++ day-1/a.py
@@ -1,11 +1,8 @@
 def main() -> None:
-    numbers = []
     with open("./input.txt", "r") as f:
         lines = f.readlines()
-        for line in lines:
-            digits = list(filter(lambda x: x.isnumeric(), list(line)))
-            numbers.append(int(digits[0] + digits[-1]))
-    print(sum(numbers))
+        digits = [list(filter(lambda x: x.isnumeric(), list(line))) for line in lines]
+        print(sum(int(digit[0] + digit[-1]) for digit in digits))
 
 
 if __name__ == "__main__":
