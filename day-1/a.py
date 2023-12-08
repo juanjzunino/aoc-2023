4dc55313b622516b4cddcad4f0692fe6b348f06f
Juanjo Zunino <juanj.zunino@gmail.com>
2023-12-08 11:01:57 -0300

    Add ruff

--- day-1/a.py
+++ day-1/a.py
@@ -1,9 +1,9 @@
 def main() -> None:
-    with open("./input.txt", "r") as f:
+    with open('./input.txt', 'r') as f:
         lines = f.readlines()
         digits = [list(filter(lambda x: x.isnumeric(), list(line))) for line in lines]
         print(sum(int(digit[0] + digit[-1]) for digit in digits))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
