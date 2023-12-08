bf5bbf52b476b42a04eb8d612049e31ef380d54a
Juanjo Zunino <juanj.zunino@gmail.com>
2023-12-08 12:56:00 -0300

    Even shorter version

--- day-1/a.py
+++ day-1/a.py
@@ -1,7 +1,6 @@
 def main() -> None:
     with open('./input.txt', 'r') as f:
-        lines = f.readlines()
-        digits = [list(filter(lambda x: x.isnumeric(), list(line))) for line in lines]
+        digits = [list(filter(lambda x: x.isnumeric(), list(line))) for line in f.readlines()]
         print(sum(int(digit[0] + digit[-1]) for digit in digits))
 
 
